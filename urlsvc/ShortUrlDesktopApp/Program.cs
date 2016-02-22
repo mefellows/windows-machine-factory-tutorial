278f01b5ebde142e7e1c6101195313dfb2aa9207
Matt Fellows <matt.fellows@onegeek.com.au>
2016-02-22 11:59:53 +1100

    Working docker compose setup

--- urlsvc/ShortUrlDesktopApp/Program.cs
+++ urlsvc/ShortUrlDesktopApp/Program.cs
@@ -10,7 +10,8 @@
         static void Main(string[] args)
         {
             ShortUrlModule artificialReference;
-            var nancyHost = new NancyHost(new Uri("http://+:8080/"));
+            // var nancyHost = new NancyHost(new Uri("http://localhost:8080/"));
+            var nancyHost = new NancyHost(new Uri("http://docker:8080/"));
             nancyHost.Start();
             Console.WriteLine ("Starting on port 8080");
             Thread.Sleep(Timeout.Infinite);
